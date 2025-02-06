66ec7550fdf9a06deb53fbf20601bcbaa8b60986
Zaraishu <tendosai@gmail.com>
2025-02-06 23:35:18 +0100

    extracted fluffy speech strings into FluffSpeak.cs

---
+++ Assets/Scripts/Fluffy Shit/FluffSpeak.cs
@@ -0,0 +1,165 @@
+public static class FluffSpeak
+{
+    /**
+     * <summary>
+     * The mare is miscarrying and panicking.
+     * </summary>
+     */
+    public const string BabbehsStayInMummah = "BABBEHS STAY IN MUMMAH! SCREEEEEEE!";
+
+    /**
+     * <summary>
+     * The fluffy's giving birth.
+     * </summary>
+     */
+    public const string BiggestPoopies = "BIGGEST POOPIES! SCREEEEEE!";
+
+    /**
+     * <summary>
+     * A chirpie baby is scared.
+     * </summary>
+     */
+    public const string ScaredChirpies = "chirp! chirp! *shiver*";
+
+    /**
+     * <summary>
+     * A cannibal fluffy is hungry, but can't find other fluffies to eat.
+     * </summary>
+     */
+    public const string DeweNuDummehsAnyWhewe = "daddeh, dewe nu dummehs anywhewe!";
+
+    /**
+     * <summary>
+     * The fluffy's screaming while falling.
+     * </summary>
+     */
+    public const string EEEEEE = "EEEEEEEE!";
+
+    /**
+     * <summary>
+     * Tiny sound when fluffies trip.
+     * </summary> 
+     */
+    public const string Eep = "eep!";
+
+    /**
+     * <summary>
+     * A blind fluffy states it doesn't want to trip while walking.
+     * </summary>
+     */
+    public const string GottaGuSwow = "gotta gu swow, nu wan' faww down...";
+
+    /**
+     * <summary>
+     * The fluffy miscarried.
+     * </summary>
+     */
+    public const string HabTummyOwies = "<name> hab tummy owwies...";
+
+    /**
+     * <summary>
+     * The fluffy is in great pain.
+     * </summary>
+     */
+    public const string HabWowstestHuwties = "<name> hab wowstest huwties... huu huu huu...";
+
+    /**
+     * <summary>
+     * Fluffy's crying to itself due to pain or hunger. 
+     * </summary>
+     */
+    public const string HuuHuuHuu = "huu huu huu...";
+
+    /**
+     * <summary>
+     * The 'mummah song' a mother or pregnant mare sing to her babies.  
+     * </summary>
+     */
+    public const string MummahSong = "mummah wub babbehs, babbehs wub mummah...";
+
+    /**
+     * <summary>
+     * A cry of severe pain or fear.
+     * </summary>
+     */
+    public const string Screee = "SCREEEEEEEEEEEEEEEEEEEEEEEEE!";
+
+    /**
+     * <summary>
+     * The fluffy's starving and begging for food.
+     * </summary>
+     */
+    public const string SuHungwy = "daddeh, pwease... su hungwy...";
+
+    /**
+     * <summary>
+     * A mare is in labor.
+     * </summary>
+     */
+    public const string TummyOwies = "TUMMY OWWIES!";
+
+    /**
+     * <summary>
+     * A fluffy is scared of an alicorn foal.
+     * </summary>
+     */
+    public const string MunstahBabbeh = "MUNSTAH BABBEH! SCREEEEEEE!";
+
+    /**
+     * <summary>
+     * A fluffy is scared of an adult alicorn.
+     * </summary>
+     */
+    public const string MunstahFwuffy = "MUNSTAH FWUFFY! SCREEEEE!";
+
+    /**
+     * <summary>
+     * The fluffy cries about being blind.
+     * </summary>
+     */
+    public const string NuCanSeeNuffin = "nu can see nuffin... huu huu huu...";
+
+    /**
+     * <summary>
+     * Another fluffy talks to the fluffy, but it can't hear.
+     * </summary>
+     */
+    public const string NuCanHeawYu = "<name> sowwy, nu can heaw yu! huu huu huu...";
+
+    /**
+     * <summary>
+     * An amputated fluffy is sad that it can't walk or run properly. 
+     * </summary>
+     */
+    public const string NuCanMakeGudWalkiesAnWunnies = "nu can make gud wawkies an' wunnies... huu huu huu...";
+
+    /**
+     * <summary>
+     * The cry of displeasure or disbelieve.
+     * </summary>
+     */
+    public const string NUUUU = "NUUUU!";
+
+    /**
+     * <summary>
+     * A pregnant mare does not want to bear children.
+     * </summary>
+     */
+    public const string NuWanHabStupidDummehBabbehs = "nu wan' hab stupid dummeh babbehs...";
+
+    public const string NuWanMunstaHuggies = "";
+
+    /**
+     * <summary>
+     * Peeping of a chirpie baby.
+     * </summary>
+     */
+    public const string PeepPeep = "peep peep!";
+
+    /**
+     * <summary>
+     * The fluffy wants to talk, but has no other fluffies around.
+     * </summary>
+     */
+    public const string WanFwends = "<name> wan' fwends...";
+}
